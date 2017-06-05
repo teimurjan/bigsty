b8f07f85217145eb469d3c48eaca29151ce5a192
teimurjan <teymurgg321@gmail.com>
2017-06-05 14:28:46 +0600

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# online-store
+# Installation
+#### Clone project
+```sh
+git clone https://github.com/teimurjan/online-store.git projects/online-store
+cd ~/projects/online-store
+```
+#### Install global dependencies
+* ##### [Yarn](https://yarnpkg.com/lang/en/docs/install/)
+* ##### [Node](https://nodejs.org/en/download/)
+* ##### [Python 3.5.2](https://www.python.org/downloads/release/python-352/)
+#### Install backend dependencies
+```sh
+source virtualenv/bin/activate
+pip install -r requirements.txt
+```
+* __Create database__
+``` sh
+    mysql -uusername -ppassword
+    create database online_store;
+    exit;
+```
+* __Make migrations__
+``` sh
+python3 manage.py makemigrations
+python3 manage.py migrate
+```
+#### Install frontend dependencies
+``` sh
+cd store/frontend
+yarn
+```
--- main/settings.py
+++ main/settings.py
@@ -29,41 +29,41 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'store',
+  # 'django.contrib.admin',
+  # 'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'store',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  # 'django.middleware.csrf.CsrfViewMiddleware',
+  # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                # 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        # 'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'main.wsgi.application'
@@ -72,14 +72,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_store',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'online_store',
+    'USER': 'root',
+    'PASSWORD': 'root',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
 
 # Password validation
