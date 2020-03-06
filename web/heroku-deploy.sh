3776b87575cf915e0bcb715fa9bd6db2243168ab
Teimur Gasanov <teymurgg321@gmail.com>
2020-03-06 10:12:23 +0600

    Infrastructure improvements

--- web/heroku-deploy.sh
+++ web/heroku-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-heroku container:push web --arg REACT_APP_SERVER_URL=https://bigsty.herokuapp.com --app bigsty-react
+heroku container:push web --app bigsty-react
 heroku container:release web --app bigsty-react
