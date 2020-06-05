5e4a68e551b4bc4266ee094a6eb431845a00bf06
Teimur Gasanov <teymurgg321@gmail.com>
2020-06-05 16:19:24 +0600

    Get rid of latest

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -2,12 +2,3 @@ const withZero = (value: number) => (value > 9 ? value : `0${value}`);
 
 export const getFormattedDateString = (date: Date) =>
   `${withZero(date.getDate())}.${withZero(date.getMonth())}.${date.getFullYear()}`;
-
-export const isToday = (date: Date) => {
-  const today = new Date();
-  return (
-    date.getDate() === today.getDate() &&
-    date.getMonth() === today.getMonth() &&
-    date.getFullYear() === today.getFullYear()
-  );
-};
--- web/src/utils/rates.ts
+++ web/src/utils/rates.ts
@@ -1,11 +1,10 @@
-import { isToday } from 'src/utils/date';
+import { getFormattedDateString } from 'src/utils/date';
 
 export const getRatesDateKey = (dateStr?: string) => {
+  const defaultKey = getFormattedDateString(new Date());
   if (dateStr) {
-    const date = new Date(dateStr);
-    date.setMonth(date.getMonth() + 1);
-    return isToday(date) ? 'latest' : dateStr;
+    return dateStr;
   }
 
-  return 'latest';
+  return defaultKey;
 };
